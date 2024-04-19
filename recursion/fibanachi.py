c348c61f1484f94997ad8e12ab4b4c7d8ae6ae81
krasnikovgp <148316952+krasnikovgp@users.noreply.github.com>
2024-04-19 15:10:58 +0300

    upgrade fibanachi

--- recursion/fibanachi.py
+++ recursion/fibanachi.py
@@ -1,8 +1,17 @@
 def fib(x):
     if x >= 2:
-        return fib(x-1) + fib(x-2)
+        return fib(x - 1) + fib(x - 2)
     else:
         return x
 
 
-print(fib(7))
+def fib_din(n):
+    if n >= len(fib_list):
+        for i in range(len(fib_list), n + 1):
+            fib_list.append(fib_din(i - 1) + fib_din(i - 2))
+    return fib_list[n]
+
+
+fib_list = [0, 1]
+
+print(fib_din(20577))  # максимльное значение
